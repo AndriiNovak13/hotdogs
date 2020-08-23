4f23acf6fa0d7a9c3a910cd2c0669a80f91d24ac
AndriiNovak13 <AndriiNovak13@users.noreply.github.com>
2020-08-23 21:02:19 +0300

    Updates

---
+++ precache-manifest.33d84b4c9cc21fc4d4d266314c664264.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a1987973d38a60bc3df927fadcd680",
+    "url": "/hotdogs/index.html"
+  },
+  {
+    "revision": "24415ff50630a7e5ade3",
+    "url": "/hotdogs/static/css/main.0b8e38d9.chunk.css"
+  },
+  {
+    "revision": "cd0c374edb3530a0a44d",
+    "url": "/hotdogs/static/js/2.f53e52b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hotdogs/static/js/2.f53e52b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24415ff50630a7e5ade3",
+    "url": "/hotdogs/static/js/main.faeccd95.chunk.js"
+  },
+  {
+    "revision": "6bd17a5b79c44b868d23",
+    "url": "/hotdogs/static/js/runtime-main.5ff2be27.js"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/hotdogs/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "260c8072feca314957defd2ad4ccc0ee",
+    "url": "/hotdogs/static/media/roboto-v20-latin-500.260c8072.eot"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/hotdogs/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "8c608256fb0273e3a36e6b603f71f213",
+    "url": "/hotdogs/static/media/roboto-v20-latin-500.8c608256.ttf"
+  },
+  {
+    "revision": "95204ac95130828753c0ee0ada537c33",
+    "url": "/hotdogs/static/media/roboto-v20-latin-500.95204ac9.svg"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/hotdogs/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "4570b93ae0caba2b938e2ddf091099ea",
+    "url": "/hotdogs/static/media/roboto-v20-latin-700.4570b93a.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/hotdogs/static/media/roboto-v20-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "96559ffb5be917f1ce9f748bf4f34732",
+    "url": "/hotdogs/static/media/roboto-v20-latin-700.96559ffb.ttf"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/hotdogs/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/hotdogs/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/hotdogs/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/hotdogs/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/hotdogs/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/hotdogs/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
